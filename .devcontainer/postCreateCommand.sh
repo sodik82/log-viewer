206ee527b3cccc35ab55bae199cb37bb202825e9
Stanislav Miklik <stanislav.miklik@gmail.com>
2026-06-18 23:33:24 +0200

    Add dev containers

---
+++ .devcontainer/postCreateCommand.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+set -e
+
+# fixes: default user does not have permissions to mounted volumes
+sudo chown -R node:node /home/node/.claude
+
+# fixes: without claude.json it cannot remember claude login from CLI
+# and it is not possible to mount a file before it is created
+[ -f /home/node/.claude/.claude.json ] || echo '{}' > /home/node/.claude/.claude.json
+ln -sf /home/node/.claude/.claude.json /home/node/.claude.json
+
+npm ci
